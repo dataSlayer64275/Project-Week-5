3837a4738978f915c47459eff7711ff3853a897a
dataSlayer64275 <ess.knst@gmail.com>
2019-04-17 14:18:29 +0100

    CSS EDITS

--- controllers/artist_controller.rb
+++ controllers/artist_controller.rb
@@ -4,16 +4,16 @@ require_relative( '../models/artist.rb' )
 
 get '/artists' do
   @artists = Artist.all
-  erb( :"artists/index" )
+  erb (:"artists/index")
 end
 
 get '/artists_manager/new' do
-  erb :"m_artists/new", :layout => :layout_manager
+  erb (:"m_artists/new"), :layout => :layout_manager
 end
 
 get '/artists_manager' do
   @artists = Artist.all
-  erb :"m_artists/manager_index", :layout => :layout_manager
+  erb (:"m_artists/manager_index"), :layout => :layout_manager
 end
 
 post '/artists_manager' do
@@ -29,12 +29,12 @@ end
 
 get '/artists_manager/:id/edit' do
   @artist = Artist.find( params[:id] )
-  erb :"m_artists/edit", :layout => :layout_manager
+  erb (:"m_artists/edit"), :layout => :layout_manager
 end
 
 post '/artists_manager/:id/delete' do
   artist = Artist.find( params[:id] )
-  artist.delete()
+  artist.delete
   redirect to '/artists_manager'
 end
 
--- controllers/exhibition_controller.rb
+++ controllers/exhibition_controller.rb
@@ -6,17 +6,17 @@ require_relative('../models/artist.rb')
 require_relative('../models/category.rb')
 
 
-get "/exhibitions/browse_by_artist" do
+get '/exhibitions/browse_by_artist' do
   @exhibitions = Exhibition.find_by_artist_id(params['artist_id'])
   @artists = Artist.all
   @category = Category.all
-  erb :"exhibitions/browse_by_artist"
+  erb (:"exhibitions/browse_by_artist")
 end
 
 get '/exhibitions' do
   @exhibitions = Exhibition.all
   @artists = Artist.all
-  erb( :"exhibitions/index" )
+  erb (:"exhibitions/index")
 end
 
 get '/exhibitions_manager' do
@@ -28,25 +28,25 @@ end
 get '/exhibitions_manager/new' do
   @categories = Category.all
   @artists = Artist.all
-  erb :"m_exhibitions/new"
+  erb (:"m_exhibitions/new")
 end
 
 post '/exhibitions_manager' do
   exhibition = Exhibition.new(params)
   exhibition.save
-  redirect to "/exhibitions_manager"
+  redirect to '/exhibitions_manager'
 end
 
 get '/exhibitions/:id' do
   @exhibition = Exhibition.find (params[:id])
-  erb :"exhibitions/show"
+  erb (:"exhibitions/show")
 end
 
 get '/exhibitions_manager/:id/edit' do
   @artists = Artist.all
   @categories = Category.all
   @exhibition = Exhibition.find( params[:id] )
-  erb(:"m_exhibitions/edit")
+  erb (:"m_exhibitions/edit")
 end
 
 
@@ -57,13 +57,12 @@ end
 
 post '/exhibitions_manager/:id' do
   exhibition = Exhibition.new(params)
-
   exhibition.update
   redirect to '/exhibitions_manager'
 end
 
 post '/exhibitions_manager/:id/delete' do
   exhibition = Exhibition.find( params[:id] )
-  exhibition.delete()
+  exhibition.delete
   redirect to '/exhibitions_manager'
 end
